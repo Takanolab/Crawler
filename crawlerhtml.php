169b45ba853039df6b717e61abf8088650758aff
ZeeSmall <small.a.big@gmail.com>
2013-05-15 12:31:47 +0900

    filter function and saveLog fanction add

--- crawlerhtml.php
+++ crawlerhtml.php
@@ -1,24 +1,55 @@
 ﻿<?php
 
+  // Warningを表示させない
+  error_reporting(0);
+
+  // 単語フィルター（仮）
   $filter = array("★");
-  function selecter($filter, $value)
+  $log = "";
+
+  // ノードの内容でフィルタ
+  function valueFilter($filter, $node)
   {
-      foreach($filter as $word) {
-          if (strstr($value, $word)) {
-              echo $value."<br>\n";
-          }
+    global $log;
+    foreach($filter as $word) {
+      if (strstr($node->nodeValue, $word)) {
+        $log .=  $node->getAttribute("href")."\n";
+        echo $node->nodeValue . "<br>\n";
       }
+    }
   }
 
-  $file = "C:/Doc/asahi.html";
+  // リンクを保存
+  function saveLogs($text)
+  {
+    $file = fopen("crowler.log", "wb");
+    // mb_language("Japanese");
+    // $text = mb_convert_encoding($text, mb_detect_encoding(), "AUTO");
+    if(fwrite($file, $text) == FALSE){
+      return 0;
+    }
+    $res = fclose($file);
+
+    if($res){
+      return 0;
+    }else{
+      return -1;
+    }
+  }
+
+  // HTMLファイル入力
+  $file = "test.html";
   $doc  = new DOMDocument();
 
   $doc->loadHTMLFile($file);
   $xpath = new DOMXpath($doc);
 
+  // ノード取り出し
   foreach($xpath->query('//a') as $node) {
-      // echo $node->getAttribute("href")." ";
-        echo $node->nodeValue."<br/>\n";
-      // selecter($filter, $node->nodeValue);
+    // $log .=  $node->getAttribute("href")."\n";
+    // echo $node->nodeValue."<br/>\n";
+    valueFilter($filter, $node);
   } 
+  saveLogs($log);
+
 ?>
